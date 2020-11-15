4903021b597aeefcacce42c53ab7c246fdfe0f8e
Nada Zeini <nada.elzeini@sjsu.edu>
2020-11-15 04:40:09 -0800

    changed pb - solution now feasible

--- main.py
+++ main.py
@@ -25,18 +25,33 @@ def get_entering_index(table, skip):
     if len(pair) == 0:
         return "no pivot"
     min_pair = min(pair, key=lambda p: p[1])
-    print(min_pair)
     entering_index = min_pair[0]
-    # entering_index = indices[0]
-    print(entering_index)
+    print("entering index ", entering_index)
     if entering_index is None:
         return "no pivot"
     return entering_index
 
 
+# row_vars = ["x1", "x2", "a1", "a2"]
+# artificial_vars = ["a1", "a2"]
+# basic_vars = ["a1", "a2"]
+row_vars = ["x", "y", "d1", "d2", "d3", "d4", "e1", "e2", "e3", "e4", "w1", "w2", "w3", "w4", "n1", "n2", "n3",
+            "n4", "s1", "s2", "s3", "s4", "a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8", "a9", "a10", "a11", "a12"]
+artificial_vars = ["a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8", "a9", "a10", "a11", "a12"]
+basic_vars = ["a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8", "a9", "a10", "a11", "a12"]
+
+
+def enter_exit_variable(table, entering_index, exiting_index):
+    enter = row_vars[entering_index]
+    exit = basic_vars[exiting_index - 1]
+    basic_vars[exiting_index - 1] = enter
+    print("enter " + row_vars[entering_index])
+    print("exit " + exit)
+    print(basic_vars)
+
+
 def get_pivot_in_iteration(table, skip):
     obj_func = table[0]
-    count = 0
     entering_index = get_entering_index(table, skip)
     if entering_index == "no pivot":
         return "no pivot"
@@ -52,14 +67,14 @@ def get_pivot_in_iteration(table, skip):
                 ratios.append(rhs_var)
             else:
                 ratios.append(rhs_var / pivot_var)
-    print("ratios ", ratios)
+    # print("ratios ", ratios)
     positive_ratios = [i for i in ratios if i != "" and i >= 0]
-    print("positive ratios ", positive_ratios)
+    # print("positive ratios ", positive_ratios)
     while len(positive_ratios) == 0:
         count = 0
         if entering_index != len(table[0]) - 2:
             skip.add(entering_index)
-            print("skip ", skip)
+            # print("skip ", skip)
         entering_index = get_entering_index(table, skip)
         if entering_index == "no pivot":
             return "no pivot"
@@ -72,12 +87,16 @@ def get_pivot_in_iteration(table, skip):
             else:
                 if rhs_var == 0:
                     ratios.append(rhs_var)
-                    print("appended 0 ", ratios)
+                    # print("appended 0 ", ratios)
                 else:
                     ratios.append(rhs_var / pivot_var)
-        print("ratios ", ratios)
+        # print("ratios ", ratios)
         positive_ratios = [i for i in ratios if i != "" and i >= 0]
-        print("positive ratios ", positive_ratios)
+        # print("positive ratios ", positive_ratios)
+    enter_exit_variable(table, entering_index, ratios.index(min(positive_ratios)) + 1)
+    print("el")
+    print(ratios.index(min(positive_ratios)) + 1)
+    print(table[ratios.index(min(positive_ratios)) + 1][entering_index])
     return entering_index, ratios.index(min(positive_ratios)) + 1
 
 
@@ -95,7 +114,7 @@ def do_pivot(table, entering_col, exiting_row, pivot):
             multiplier = row[entering_col]
         for c, var in enumerate(row):
             if r != exiting_row:
-                table[r][c] = var - multiplier * table[exiting_row][c]
+                table[r][c] = var - (multiplier * table[exiting_row][c])
 
 
 def next_iteration(table):
@@ -196,12 +215,20 @@ def two_phase_simplex(table, obj_func, total_vars, total_art_vars):
 def make_ready_for_simplex(table):
     # get positions of pivots
     positions = []
-    for col in range(0, len(table[0]) - 1):
-        row = col + 1
-        position = (row, col)
-        positions.append(position)
+    for var in row_vars:
+        if var in basic_vars:
+            positions.append((basic_vars.index(var) + 1, row_vars.index(var)))
+    # for col in range(0, len(table[0]) - 1):
+    #     row = col + 1
+    #     position = (row, col)
+    #     positions.append(position)
+    # print(positions)
+    # print(row_vars)
+    # print(basic_vars)
     for (row, col) in positions:
+        # print(row, col, table[row][col], sep="  ")
         do_pivot(table, exiting_row=row, entering_col=col, pivot=table[row][col])
+    # return positions
 
 
 def phase2(table, obj_func, total_vars, total_art_vars):
@@ -213,7 +240,9 @@ def phase2(table, obj_func, total_vars, total_art_vars):
     new_table = new_table.tolist()
     # before simplex method need to make it ready - by pivoting
     print_table(new_table)
-    make_ready_for_simplex(new_table)
+    make_ready_for_simplex(new_table)  # should make ready the basic variable (in first column)
+    print("ready for simplex now")
+    print_table(new_table)
     simplex_method(new_table)
 
 
@@ -221,7 +250,6 @@ if __name__ == "__main__":
     objective_func = [-5, -1]
     z_prime = [0, 0, 1, 1, 0]
     first_table = [z_prime, [1, -3, 1, 0, 1], [2, -1, 0, 1, 3]]
-    # two_phase_simplex(first_table, obj_func=objective_func, total_art_vars=2, total_vars=4)
 
     # variables = int(input("Enter the number of variables: "))
     # constraints = int(input("Enter the number of constraints: "))
@@ -231,22 +259,24 @@ if __name__ == "__main__":
     # variables = ["d1", "d2", "d3", "d4"]
     artificial_variables = []
     const_num = 8
-    z = [0, 0, -20, -30, -40, -25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    # two_phase_simplex(first_table, obj_func=objective_func, total_art_vars=2, total_vars=4)
+    z = [0, 0, 20, 30, 40, 25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     z_prime = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
                0]
     pb = [z_prime,
-          # x, y, d1 d2 d3 d4 e1 w1 n1 s1 e1 w1 n1 s1  e1 w1 n1 s1  e1 w1 n1 s1  e1 w1 n1 s1 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12
-          [0, 0, 1, 0, 0, 0, -1, -1, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, -1, -1, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1, -1, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1, -1, -1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-          [1, 0, 0, 0, 0, 0, 1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 10],
-          [0, 1, 0, 0, 0, 0, 0, 0, -1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 20],
-          [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 60],
-          [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 20],
-          [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 40],
-          [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 30],
-          [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 80],
-          [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 60]
+          # x, yd1 d2 d3d4 || e w n s
+          [1, 0, 0, 0, 0, 0, -1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10],
+          [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, -1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 20],
+          [1, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 60],
+          [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, -1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 20],
+          [1, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 40],
+          [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 30],
+          [1, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 80],
+          [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 60],
+          [0, 0, 1, 0, 0, 0, -1, 0, 0, 0, -1, 0, 0, 0, -1, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+          [0, 0, 0, 1, 0, 0, 0, -1, 0, 0, 0, -1, 0, 0, 0, -1, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+          [0, 0, 0, 0, 1, 0, 0, 0, -1, 0, 0, 0, -1, 0, 0, 0, -1, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+          [0, 0, 0, 0, 0, 1, 0, 0, 0, -1, 0, 0, 0, -1, 0, 0, 0, -1, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
           ]
     two_phase_simplex(table=pb, obj_func=z, total_vars=34, total_art_vars=12)
